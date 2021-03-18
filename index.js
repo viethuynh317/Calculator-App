89d66afdf63c94f0b8ba0462625059b4f0fd8265
Viet huynh <huynhvanviet317@gmail.com>
2021-03-18 21:41:05 +0700

    Build base UI and setup extract HTML DOM

---
+++ index.js
@@ -0,0 +1,43 @@
+// initial variable DOM FUNC
+const acFuncEle = document.querySelector('.ac');
+const pmFuncEle = document.querySelector('.pm');
+const percFuncEle = document.querySelector('.percent');
+// ---------------------
+
+// initial variable DOM OPERATOR
+const divOperatorEle = document.querySelector('.div');
+
+const mulOperatorEle = document.querySelector('.mul');
+
+const subOperatorEle = document.querySelector('.sub');
+
+const sumOperatorEle = document.querySelector('.sum');
+
+const equalOperatorEle = document.querySelector('.equal');
+
+//-----------------------
+
+// initial variable DOM NUMBER
+
+const numberZeroEle = document.querySelector('.number-0');
+
+const numberOneEle = document.querySelector('.number-1');
+
+const numberTwoEle = document.querySelector('.number-2');
+
+const numberThreeEle = document.querySelector('.number-3');
+
+const numberFourEle = document.querySelector('.number-4');
+
+const numberFiveEle = document.querySelector('.number-5');
+
+const numberSixEle = document.querySelector('.number-6');
+
+const numberSevenEle = document.querySelector('.number-7');
+
+const numberEightEle = document.querySelector('.number-8');
+
+const numberNineEle = document.querySelector('.number-9');
+
+// ------------------------
+
